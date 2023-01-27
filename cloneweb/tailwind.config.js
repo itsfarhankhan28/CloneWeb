182b83028fa634405fbf57a52985e58f2f1bb296
farhan <famannat@gmail.com>
2023-01-27 17:53:49 +0530

    sm min-width changed

--- cloneweb/tailwind.config.js
+++ cloneweb/tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
       '5xl': ['45px'],
       '6xl': ['65px'],
       '10xl': ['128px']
+    },
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [],
