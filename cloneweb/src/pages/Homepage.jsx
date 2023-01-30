d5b113f226c7d50caf3fd04b060f24dbe765faeb
farhan <famannat@gmail.com>
2023-01-31 00:20:07 +0530

    other cards changed

--- cloneweb/src/pages/Homepage.jsx
+++ cloneweb/src/pages/Homepage.jsx
@@ -68,7 +68,7 @@ const Homepage = () => {
                 </div>
                 <div className='grid grid-cols-2'>
                   <div>
-                    <h1 className='lg:text-xl sm:text-lg relative lg:left-[180px] lg:top-[10px] font-bold sm:top-[8px] sm:left-[135px]'>4.7</h1>
+                    <h1 className='lg:text-xl sm:text-lg relative lg:left-[180px] lg:top-[10px] font-bold sm:top-[8px] md:left-[145px] sm:left-[135px]'>4.7</h1>
                   </div>
                   <div className='lg:text-3xl sm:text-xl lg:pt-[7px] lg:pl-[15px] sm:pt-[9px] sm:pl-[12px]'>
                     <ion-icon name="star"></ion-icon>
@@ -76,17 +76,57 @@ const Homepage = () => {
                 </div>
               </div>
             </div>
+
             <div className='lg:h-[370px] sm:h-[330px] shadow-2xl'>
               <img className='w-[473px]' src={hotel2} alt="" />
-              <h1 className='text-xl font-semibold pt-[12px] pl-[6px]'>Burj Al Arab</h1>
-              <h1 className='text-[20px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
+              <div className='grid lg:grid-cols-2 sm:grid-cols-2 md:grid-cols-2'>
+                <div>
+                  <h1 className='lg:text-xl sm:text-base font-semibold pt-[12px] pl-[6px]'>Burj Al Arab</h1>
+                  <h1 className='lg:text-[20px] sm:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
+                  <div className='w-[111px] h-[35px] bg-red-500 rounded-[10px] mt-[10px] ml-[6px]'>
+                    <h1 className='text-white font-semibold text-[18px] pl-[13px] pt-[4px]'>Check out</h1>
+                  </div>
+                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] sm:bottom-[40px] sm:left-[135px]'>
+                  <ion-icon name="heart-outline"></ion-icon>
+                  </div>
+                </div>
+                <div className='grid grid-cols-2'>
+                  <div>
+                    <h1 className='lg:text-xl sm:text-lg relative lg:left-[180px] lg:top-[10px] font-bold sm:top-[8px] md:left-[145px] sm:left-[135px]'>5.0</h1>
+                  </div>
+                  <div className='lg:text-3xl sm:text-xl lg:pt-[7px] lg:pl-[15px] sm:pt-[9px] sm:pl-[12px]'>
+                    <ion-icon name="star"></ion-icon>
+                  </div>
+                </div>
+              </div>
             </div>
+
             <div className='lg:h-[370px] sm:h-[330px] shadow-2xl'>
               <img className='w-[473px]' src={hotel3} alt="" />
-              <h1 className='text-xl font-semibold pt-[12px] pl-[6px]'>Oberoi Hotel</h1>
-              <h1 className='text-[20px] font-semibold pt-[5px] pl-[6px]'>Rs.7000 Per person</h1>
+              <div className='grid lg:grid-cols-2 sm:grid-cols-2 md:grid-cols-2'>
+                <div>
+                  <h1 className='lg:text-xl sm:text-base font-semibold pt-[12px] pl-[6px]'>Oberoi Hotel</h1>
+                  <h1 className='lg:text-[20px] sm:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.7000 Per person</h1>
+                  <div className='w-[111px] h-[35px] bg-red-500 rounded-[10px] mt-[10px] ml-[6px]'>
+                    <h1 className='text-white font-semibold text-[18px] pl-[13px] pt-[4px]'>Check out</h1>
+                  </div>
+                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] sm:bottom-[40px] sm:left-[135px]'>
+                  <ion-icon name="heart-outline"></ion-icon>
+                  </div>
+                </div>
+                <div className='grid grid-cols-2'>
+                  <div>
+                    <h1 className='lg:text-xl sm:text-lg relative lg:left-[180px] lg:top-[10px] font-bold sm:top-[8px] md:left-[145px] sm:left-[135px]'>4.8</h1>
+                  </div>
+                  <div className='lg:text-3xl sm:text-xl lg:pt-[7px] lg:pl-[15px] sm:pt-[9px] sm:pl-[12px]'>
+                    <ion-icon name="star"></ion-icon>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
+
         </div>
     </div>
   )
