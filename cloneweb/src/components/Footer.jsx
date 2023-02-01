b72530b61fb36df1a2c480b219a43bfea69ae7c0
farhan <famannat@gmail.com>
2023-02-02 01:24:55 +0530

    md screen footer

--- cloneweb/src/components/Footer.jsx
+++ cloneweb/src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <>
       <div className='max-w-[100%] mt-[100px] h-[460px] bg-gray-200'>
         <div className='max-w-[1500px] mx-auto h-[430px] relative top-[30px]'>
-          <div className='grid grid-cols-4'>
+          <div className='grid lg:grid-cols-4 md:grid-cols-1'>
             <div className=''>
               <h1 className='font-semibold text-xl'>Support</h1>
-              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1'>
+              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1 md:grid-cols-3'>
                 <div>Help center</div>
                 <div>Air cover</div>
                 <div>Supporting people with disabilities</div>
@@ -19,14 +19,14 @@ const Footer = () => {
             </div>
             <div className=''>
               <h1 className='font-semibold text-xl'>Community</h1>
-              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1'>
+              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1 md:grid-cols-2'>
                 <div>Airbnb.org: disaster relief housing</div>
                 <div>Combating discrimination</div>
               </div>
             </div>
             <div className=''>
               <h1 className='font-semibold text-xl'>Hosting</h1>
-              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1'>
+              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1 md:grid-cols-3'>
                 <div>Airbnb your home</div>
                 <div>AirCover for Hosts</div>
                 <div>Explore hosting resources</div>
@@ -36,7 +36,7 @@ const Footer = () => {
             </div>
             <div className=''>
               <h1 className='font-semibold text-xl'>Airbnb</h1>
-              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1'>
+              <div className='text-sm pt-[20px] leading-[45px] grid lg:grid-cols-1 md:grid-cols-3'>
                 <div>Newsroom</div>
                 <div>Learn about new features</div>
                 <div>Letter from our founders</div>
@@ -48,8 +48,8 @@ const Footer = () => {
 
           <div className='max-w-[1500px] border-solid border-2 border-gray-300 mt-[50px] '></div>
 
-          <div className='grid grid-cols-2'>
-            <div className='text-sm mt-3'>2023 Airbnb,Inc,   Privacy   Terms   Sitemap   Company details</div>
+          <div className='grid lg:grid-cols-2 md:grid-cols-1'>
+            <div className='text-sm mt-3 lg:text-left md:text-center'>2023 Airbnb,Inc,   Privacy   Terms   Sitemap   Company details</div>
             <div className='grid grid-cols-3 mt-3'>
               <div className='flex pl-[420px]'>
                 <div className='text-[25px]'>
