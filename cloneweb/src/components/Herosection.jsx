de6a21e9576ce8d59df7a3b5015bd8fe91339afa
farhan <famannat@gmail.com>
2023-02-02 18:00:22 +0530

    small screen leading changed

--- cloneweb/src/components/Herosection.jsx
+++ cloneweb/src/components/Herosection.jsx
@@ -6,7 +6,7 @@ const Herosection = () => {
         <div className='max-w-[1500px] mx-auto '>
             <div className='md:text-center lg:text-left sm:text-center lg:leading-[150px]'>
               <h1 className='text-red-500 lg:text-10xl sm:text-7xl font-semibold pt-[150px]'>Airbnb</h1>
-              <h2 className='lg:text-6xl lg:m-0 sm:text-4xl lg:leading-[65px] sm:leading-[45px] font-semibold leading-[65px]'>Book unique homes and <br />  experience a city like a local.</h2>
+              <h2 className='lg:text-6xl lg:m-0 sm:text-4xl lg:leading-[65px] sm:leading-[43px] font-semibold leading-[65px]'>Book unique homes and <br />  experience a city like a local.</h2>
             </div>
         </div>
         <div className='lg:max-w-[1500px] lg:mx-auto sm:mx-2 lg:h-[70px] sm:h-[50px] relative top-[53px] shadow-xl flex'>
