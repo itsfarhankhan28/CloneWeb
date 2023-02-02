838e9da44d5aa77c9ea78db049217e55d205ee3c
farhan <famannat@gmail.com>
2023-02-02 18:14:54 +0530

    small screen card height changed

--- cloneweb/src/components/ExploreAirbnb.jsx
+++ cloneweb/src/components/ExploreAirbnb.jsx
@@ -8,7 +8,7 @@ const ExploreAirbnb = () => {
     <>
       <div className='max-w-[1500px] mx-auto pt-[150px]'>
           <h1 className='font-semibold lg:text-5xl sm:text-3xl lg:m-0 sm:ml-2'>Explore Airbnb</h1>
-          <div className='max-w-[1000px] text-xl grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:gap-10 sm:gap-10 md:gap-10 pt-3'>
+          <div className='max-w-[1000px] text-xl grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:gap-10 sm:gap-5 md:gap-10 pt-3'>
             <div className='flex border-solid border-2 lg:shadow-2xl sm:shadow-xl lg:m-0 sm:mx-2'>
               <img className='h-[95px] sm:h-[70px]' src={hotels} alt="" />
               <h1 className='relative lg:top-[30px] lg:left-[15px] sm:top-[18px] sm:left-[10px]'>Hotels</h1>
