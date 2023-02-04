755ea4857c1909316a7ceb176122a2a8134e8036
farhan <famannat@gmail.com>
2023-02-04 13:30:06 +0530

    featured destination according to small screen

--- cloneweb/src/components/FeaturedDest.jsx
+++ cloneweb/src/components/FeaturedDest.jsx
@@ -10,19 +10,19 @@ const FeaturedDest = () => {
       <div className='max-w-[1500px] mx-auto pt-[100px]'>
           <h1 className='font-semibold lg:text-5xl sm:text-3xl'>Featured Destinations</h1>
           <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-10 pt-3'>
-            <div className='shadow-2xl'>
+            <div className='shadow-2xl lg:m-0 sm:mx-2'>
               <img className='w-[400px]' src={dest1} alt="" />
               <h1 className='text-3xl font-semibold pl-[15px]'>India</h1>
             </div>
-            <div className='shadow-2xl'>
+            <div className='shadow-2xl lg:m-0 sm:mx-2'>
               <img className='w-[400px]' src={dest2} alt="" />
               <h1 className='text-3xl font-semibold pl-[15px]'>Dubai</h1>
             </div>
-            <div className='shadow-2xl'>
+            <div className='shadow-2xl lg:m-0 sm:mx-2'>
               <img className='w-[400px]' src={dest3} alt="" />
               <h1 className='text-3xl font-semibold pl-[15px]'>Paris</h1>
             </div>
-            <div className='shadow-2xl'>
+            <div className='shadow-2xl lg:m-0 sm:mx-2'>
               <img className='w-[400px]' src={dest4} alt="" />
               <h1 className='text-3xl font-semibold pl-[15px]'>Egypt</h1>
             </div>
