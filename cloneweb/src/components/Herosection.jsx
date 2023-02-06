b715ecf246a7f0cc37a67bf44b52722cf5b92aca
farhan <famannat@gmail.com>
2023-02-07 00:08:03 +0530

    hamburger positionc changed in ps

--- cloneweb/src/components/Herosection.jsx
+++ cloneweb/src/components/Herosection.jsx
@@ -4,16 +4,16 @@ const Herosection = () => {
   return (
     <>
         <div className='max-w-[1500px] mx-auto '>
-            <div className='md:text-center lg:text-left pm:text-center lg:leading-[150px]'>
-              <h1 className='text-red-500 lg:text-10xl pm:text-7xl font-semibold pt-[150px]'>Airbnb</h1>
-              <h2 className='lg:text-6xl lg:m-0 pm:text-4xl lg:leading-[65px] pm:leading-[43px] font-semibold leading-[65px]'>Book unique homes and <br />  experience a city like a local.</h2>
+            <div className='md:text-center lg:text-left ps:text-center lg:leading-[150px]'>
+              <h1 className='text-red-500 lg:text-10xl ps:text-7xl font-semibold pt-[150px]'>Airbnb</h1>
+              <h2 className='lg:text-6xl lg:m-0 ps:text-4xl lg:leading-[65px] ps:leading-[43px] font-semibold leading-[65px]'>Book unique homes and <br />  experience a city like a local.</h2>
             </div>
         </div>
-        <div className='lg:max-w-[1500px] lg:mx-auto pm:mx-2 lg:h-[70px] pm:h-[50px] relative top-[53px] shadow-xl flex'>
-            <div className='lg:pt-[18px] lg:pl-[15px] lg:text-4xl pm:text-xl pm:pt-[11px]'>
+        <div className='lg:max-w-[1500px] lg:mx-auto ps:mx-2 lg:h-[70px] ps:h-[50px] relative top-[53px] shadow-xl flex'>
+            <div className='lg:pt-[18px] lg:pl-[15px] lg:text-4xl ps:text-xl ps:pt-[11px]'>
               <ion-icon name="search-outline"></ion-icon>
             </div>
-            <h1 className='lg:text-xl pm:text-base font-semibold text-gray-400 lg:pt-[25px] lg:pl-[20px] pm:pt-[11px] pm:pl-[5px]'>Try "Osaka"</h1>
+            <h1 className='lg:text-xl ps:text-base font-semibold text-gray-400 lg:pt-[25px] lg:pl-[20px] ps:pt-[11px] ps:pl-[5px]'>Try "Osaka"</h1>
         </div>
     </>
   )
--- cloneweb/src/components/Homes.jsx
+++ cloneweb/src/components/Homes.jsx
@@ -7,72 +7,72 @@ const Homes = () => {
   return (
     <>
       <div className='max-w-[1500px] mx-auto pt-[100px]'>
-          <h1 className='font-semibold lg:text-5xl pm:text-3xl lg:m-0 pm:ml-2'>Homes</h1>
-          <div className='max-w-[1500px] grid lg:grid-cols-3 md:grid-cols-2 pm:grid-cols-1 gap-10 pt-3'>
-            <div className='lg:h-[370px] pm:h-[320px] shadow-2xl lg:m-0 pm:mx-2'>
+          <h1 className='font-semibold lg:text-5xl ps:text-3xl lg:m-0 ps:ml-2'>Homes</h1>
+          <div className='max-w-[1500px] grid lg:grid-cols-3 md:grid-cols-2 ps:grid-cols-1 gap-10 pt-3'>
+            <div className='lg:h-[370px] ps:h-[320px] shadow-2xl lg:m-0 ps:mx-2'>
               <img className='w-[473px]' src={hotel1} alt="" />
-              <div className='grid lg:grid-cols-2 pm:grid-cols-2 md:grid-cols-2'>
+              <div className='grid lg:grid-cols-2 ps:grid-cols-2 md:grid-cols-2'>
                 <div>
-                  <h1 className='lg:text-xl pm:text-base font-semibold pt-[12px] pl-[6px]'>Hotel Taj Lands End</h1>
-                  <h1 className='lg:text-[20px] pm:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.5000 Per person</h1>
+                  <h1 className='lg:text-xl ps:text-base font-semibold pt-[12px] pl-[6px]'>Hotel Taj Lands End</h1>
+                  <h1 className='lg:text-[20px] ps:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.5000 Per person</h1>
                   <div className='w-[111px] h-[35px] bg-red-500 rounded-[10px] mt-[10px] ml-[6px]'>
                     <h1 className='text-white font-semibold text-[18px] pl-[13px] pt-[4px]'>Check out</h1>
                   </div>
-                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] pm:bottom-[40px] pm:left-[135px]'>
+                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] ps:bottom-[40px] ps:left-[135px]'>
                   <ion-icon name="heart-outline"></ion-icon>
                   </div>
                 </div>
                 <div className='grid grid-cols-2'>
                   <div>
-                    <h1 className='lg:text-xl pm:text-lg lg:pl-[175px] font-bold  md:left-[145px] pm:pt-[8px] pm:pl-[125px]  '>4.7</h1>
+                    <h1 className='lg:text-xl ps:text-lg lg:pl-[175px] font-bold  md:left-[145px] ps:pt-[8px] ps:pl-[125px]  '>4.7</h1>
                   </div>
-                  <div className='lg:text-3xl pm:text-xl lg:pt-[7px] lg:pl-[15px] pm:pt-[9px] pm:pl-[12px]'>
+                  <div className='lg:text-3xl ps:text-xl lg:pt-[7px] lg:pl-[15px] ps:pt-[9px] ps:pl-[12px]'>
                     <ion-icon name="star"></ion-icon>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='lg:h-[370px] pm:h-[320px] shadow-2xl lg:m-0 pm:mx-2'>
+            <div className='lg:h-[370px] ps:h-[320px] shadow-2xl lg:m-0 ps:mx-2'>
               <img className='w-[473px]' src={hotel2} alt="" />
-              <div className='grid lg:grid-cols-2 pm:grid-cols-2 md:grid-cols-2'>
+              <div className='grid lg:grid-cols-2 ps:grid-cols-2 md:grid-cols-2'>
                 <div>
-                  <h1 className='lg:text-xl pm:text-base font-semibold pt-[12px] pl-[6px]'>Burj Al Arab</h1>
-                  <h1 className='lg:text-[20px] pm:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
+                  <h1 className='lg:text-xl ps:text-base font-semibold pt-[12px] pl-[6px]'>Burj Al Arab</h1>
+                  <h1 className='lg:text-[20px] ps:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
                   <div className='w-[111px] h-[35px] bg-red-500 rounded-[10px] mt-[10px] ml-[6px]'>
                     <h1 className='text-white font-semibold text-[18px] pl-[13px] pt-[4px]'>Check out</h1>
                   </div>
-                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] pm:bottom-[40px] pm:left-[135px]'>
+                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] ps:bottom-[40px] ps:left-[135px]'>
                   <ion-icon name="heart-outline"></ion-icon>
                   </div>
                 </div>
                 <div className='grid grid-cols-2'>
                   <div>
-                    <h1 className='lg:text-xl pm:text-lg lg:pl-[175px] font-bold  md:left-[145px] pm:pt-[8px] pm:pl-[125px]  '>5.0</h1>
+                    <h1 className='lg:text-xl ps:text-lg lg:pl-[175px] font-bold  md:left-[145px] ps:pt-[8px] ps:pl-[125px]  '>5.0</h1>
                   </div>
-                  <div className='lg:text-3xl pm:text-xl lg:pt-[7px] lg:pl-[15px] pm:pt-[9px] pm:pl-[12px]'>
+                  <div className='lg:text-3xl ps:text-xl lg:pt-[7px] lg:pl-[15px] ps:pt-[9px] ps:pl-[12px]'>
                     <ion-icon name="star"></ion-icon>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='lg:h-[370px] pm:h-[320px] shadow-2xl lg:m-0 pm:mx-2'>
+            <div className='lg:h-[370px] ps:h-[320px] shadow-2xl lg:m-0 ps:mx-2'>
               <img className='w-[473px]' src={hotel3} alt="" />
-              <div className='grid lg:grid-cols-2 pm:grid-cols-2 md:grid-cols-2'>
+              <div className='grid lg:grid-cols-2 ps:grid-cols-2 md:grid-cols-2'>
                 <div>
-                  <h1 className='lg:text-xl pm:text-base font-semibold pt-[12px] pl-[6px]'>Oberoi Hotel</h1>
-                  <h1 className='lg:text-[20px] pm:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
+                  <h1 className='lg:text-xl ps:text-base font-semibold pt-[12px] pl-[6px]'>Oberoi Hotel</h1>
+                  <h1 className='lg:text-[20px] ps:text[15px] font-semibold pt-[5px] pl-[6px]'>Rs.10,000 Per person</h1>
                   <div className='w-[111px] h-[35px] bg-red-500 rounded-[10px] mt-[10px] ml-[6px]'>
                     <h1 className='text-white font-semibold text-[18px] pl-[13px] pt-[4px]'>Check out</h1>
                   </div>
-                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] pm:bottom-[40px] pm:left-[135px]'>
+                  <div className='text-3xl relative lg:bottom-[43px] lg:left-[140px] ps:bottom-[40px] ps:left-[135px]'>
                   <ion-icon name="heart-outline"></ion-icon>
                   </div>
                 </div>
                 <div className='grid grid-cols-2'>
                   <div>
-                    <h1 className='lg:text-xl pm:text-lg lg:pl-[175px]  font-bold  md:left-[145px] pm:pt-[8px] pm:pl-[125px]  '>4.8</h1>
+                    <h1 className='lg:text-xl ps:text-lg lg:pl-[175px]  font-bold  md:left-[145px] ps:pt-[8px] ps:pl-[125px]  '>4.8</h1>
                   </div>
-                  <div className='lg:text-3xl pm:text-xl lg:pt-[7px] lg:pl-[15px] pm:pt-[9px] pm:pl-[12px]'>
+                  <div className='lg:text-3xl ps:text-xl lg:pt-[7px] lg:pl-[15px] ps:pt-[9px] ps:pl-[12px]'>
                     <ion-icon name="star"></ion-icon>
                   </div>
                 </div>
