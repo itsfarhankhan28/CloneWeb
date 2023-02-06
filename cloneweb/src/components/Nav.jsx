7cf1df551fc16eb32e9245cdbe7d68cffb608bfa
farhan <famannat@gmail.com>
2023-02-07 01:10:34 +0530

    hamburger menu position refactored

--- cloneweb/src/components/Nav.jsx
+++ cloneweb/src/components/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
         <img className='h-[50px]' src={logo} alt="" />
       </div>
       
-      <div onClick={()=>setOpen(!open)} className='ps:text-2xl absolute ps:left-[300px] pm:left-[320px] top-5 cursor-pointer lg:hidden md:hidden'>
+      <div onClick={()=>setOpen(!open)} className='ps:text-2xl absolute ps:left-[320px] pm:left-[320px] top-5 cursor-pointer lg:hidden md:hidden'>
       {/* <ion-icon name={open ? 'close':'menu'}></ion-icon> */}
       <ion-icon name={open ? 'close' : 'menu-outline'}></ion-icon>
       </div>
